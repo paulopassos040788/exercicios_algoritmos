678e6c7fdabb6c21da03985f04e5fc8b4b66827a
Paulo César dos Passos Pereira <paulo.passos1@gmail.com>
2024-07-22 08:35:55 -0300

    Trabalhando com Conjuntos (Sets)
     - Crie um conjunto de nomes de cidades.
       - Adicione algumas cidades ao conjunto (incluindo repetições).
       - Verifique se uma determinada cidade está no conjunto.
       - Imprima todas as cidades do conjunto em ordem alfabética.

---
+++ src/exercicios/colecoes_genericas/TrabalhandoConjuntos.java
@@ -0,0 +1,41 @@
+package exercicios.colecoes_genericas;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class TrabalhandoConjuntos {
+
+    private final static String[] cidades = {"Sao Paulo", "Sao Paulo", "Sao Paulo", "Goiania", "Curitiba", "Salvador", "Trindade"};
+    private final static Set<String> cidadesSet = new HashSet<>();
+
+    public static void main(String[] args) {
+
+        adicionaCidade(cidades);
+
+        pesquisaCidade("Goiania");
+
+        imprimeCidades(cidadesSet);
+
+    }
+
+    private static void adicionaCidade(String[] cidades) {
+        cidadesSet.addAll(Arrays.asList(cidades));
+    }
+
+    private static void pesquisaCidade(String cidade) {
+        if (cidadesSet.contains(cidade)){
+            System.out.printf("Cidade de %s contem na lista", cidade);
+        } else {
+            System.out.println("cidade nao encontrada");
+        }
+    }
+
+    private static void imprimeCidades(Set<String> cidades) {
+        System.out.println("\nLista de cidades: ");
+        for (String cidade : cidades) {
+            System.out.println(cidade);
+        }
+    }
+
+}
